02a8fe5f73a07e97bd422ba73966ca87eb0fa0b4
John David Calimpong <johndavid.calimpong@cit.edu>
2026-04-24 04:08:59 +0800

    refine UI, fix animations, and migrate to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   }
